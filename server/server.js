7e2e34aaf9ddde2ad82a0a3ecfd87bbd231e9356
Luis Martinez <luis_mmartinez@live.com>
2021-10-24 17:00:14 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,54 @@
+// const express = require("express");
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
+// const prerender = require('prerender');
+// const server = prerender();
+// server.start();
+
+
+
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+
+import App from "../src/App";
+
+const PORT = 8000;
+
+const app = express();
+
+app.use("^/$", (req, res, next) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    );
+  });
+});
+
+// app.use(express.static(path.resolve(__dirname, '..', 'build')))
+app.use(
+    require("prerender-node").set("prerenderToken", '7Ulx7JiX3yGWkoqJqwFo')
+    );
+
+app.listen(PORT, () => {
+  console.log(`App launched on ${PORT}`);
+});
+
+
---
+++ src/App.js
@@ -0,0 +1,51 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import { Helmet } from "react-helmet";
+
+
+function App() {
+
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+           <Helmet>
+                <meta property="og:title" content="Updated!!" />
+                <title>DevBlog</title>
+                <meta name="description" content="Blog website for coding related posts" />
+                <meta name="image" content="https://user-images.githubusercontent.com/75151961/138567246-01b18138-9eb4-4d64-973b-7965083a26a8.png" />
+          </Helmet>
+          <h1>HELLO</h1>
+          <>
+          <p>{count}</p>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          </>
+    </div>
+  );
+}
+
+export default App;
