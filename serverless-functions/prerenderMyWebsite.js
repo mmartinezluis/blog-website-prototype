117acc0e16978a6a382eb66f8b979da624e8f9f2
Luis Martinez <luis_mmartinez@live.com>
2021-10-30 11:52:50 -0400

    add serverless function for Netlify

---
+++ serverless-functions/prerenderMyWebsite.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+exports.handler = async function(event, context) {
+    const prerender = require('prerender');
+    const server = prerender();
+    server.start();
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Started Prerender.io server"})
+    };
+}
+
