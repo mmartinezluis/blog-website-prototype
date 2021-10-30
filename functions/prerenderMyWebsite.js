03bb852d4316010d600905115c88e3d9495a47dd
Luis Martinez <luis_mmartinez@live.com>
2021-10-30 16:36:57 -0400

    modified files

--- functions/prerenderMyWebsite.js
+++ functions/prerenderMyWebsite.js
@@ -1,14 +1,13 @@
 exports.handler = async function(event, context) {
-    // const prerender = require('./lib');
-    // const prerender = require('prerender');
-    // const server = prerender({
-    //     chromeLocation: '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
-    // });
-    // server.start();
-
-    const prerender = require('prerender');
-    const server = prerender();
+    const prerender = require('./lib');
+    const server = prerender({
+        chromeLocation: '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
+    });
     server.start();
+
+    // const prerender = require('prerender');
+    // const server = prerender();
+    // server.start();
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Started Prerender.io server"})
