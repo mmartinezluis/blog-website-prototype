9e8600d3a57ff2960e60c387eb0bbe711d408dff
Luis Martinez <luis_mmartinez@live.com>
2021-11-27 23:46:59 -0500

    removed unneeded files

--- src/mockApi/mockApi.js
+++ src/mockApi/mockApi.js
@@ -1,5 +1,4 @@
 let dataBase = require('../db.json')
-const fs = require('fs')
 
 export function mockAPI(request) {
     return new Promise((resolve, reject) => {
@@ -7,21 +6,6 @@ export function mockAPI(request) {
         switch (request.method) {
           case 'get':
               resolve({ status: 200, data: dataBase });
-            break;
-          case 'addPost':
-              const id = dataBase.posts.length + 1;
-              const post = JSON.stringify(Object.assign({}, {id}, request.body), null, 2)
-            //   debugger
-              fs.writeFile('../database/mockDatabase.json', post, err => {
-                if (err) {
-                    console.log('Error writing file', err)
-                } else {
-                    console.log('Successfully wrote file')
-                }
-              })
-            //   debugger
-            //   dataBase.posts.push(post)
-              resolve({ status: 200, message: 'Added Post' });
             break;
           default:
             resolve({ status: 400, message: 'Bad Request' });
@@ -31,10 +15,6 @@ export function mockAPI(request) {
 }
 
 
-
-
-
-//   export default mockAPI;
 
 // function mockAPI(request) {
 //     return new Promise((resolve, reject) => {
